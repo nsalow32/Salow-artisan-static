d564ae29f87563d9d6efcfdc1cbcbf2f1528e672
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-03-28 16:43:11 -0500

    Update about.blade.php
    updated twitter to linkedin

--- source/about.blade.php
+++ source/about.blade.php
@@ -10,7 +10,7 @@
     <h2>Links:</h2>
 
     <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
+        <li><a href="/linkedin" target="_blank">LinkedIn</a></li>
         <li><a href="/github" target="_blank">GitHub</a></li>
     </ul>
 @endsection
