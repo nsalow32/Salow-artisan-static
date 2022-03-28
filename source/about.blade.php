d75139121ab8f3af5cd0963713c62626f814bbb6
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-03-28 16:58:31 -0500

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,16 +1,3 @@
 @extends('_layouts.master')
 
 @section('title', 'About')
-
-@section('content')
-    <h1>About</h1>
-
-    <p>My name is {{ $page->owner->name }}</p>
-
-    <h2>Links:</h2>
-
-    <ul>
-        <li><a href="/linkedin" target="_blank">LinkedIn</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
-    </ul>
-@endsection
