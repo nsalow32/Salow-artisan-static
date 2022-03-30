98dfe8ba064865017e2b0b57909d39381a24fd3f
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-03-30 09:32:44 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,19 +1,24 @@
 @extends('_layouts.master')
 
 @section('content')
-    <NMblack>>HELLO! MY NAME IS<NMblack><NM> NICOLE SALOW.</NM>
+    <NMblack>>HELLO! MY NAME IS<NMblack><NM>NICOLE SALOW.</NM>
     </br>
     <NMblack>Let's work together!</NMblack>
+    
+    <body>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
+        <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
 
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
+        <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
 
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
+        <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
 
-    <h2 id="test">Asset Compilation Test</h2>
+        <h2 id="test">Asset Compilation Test</h2>
 
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
+        <p>CSS test: <span class="test-css">text with blue border</span></p>
 
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+        <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+        
+    </body>
+    
 @endsection
