79dc469a926eade9aef81b602d07a66b8216744d
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-03-30 09:39:01 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,22 +1,17 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>HELLO! MY NAME IS<h1><h2>NICOLE SALOW.</h2>
-    </br>
-    <h2>Let's work together!</h2>
+    <h1>HELLO! MY NAME IS<h1><h2>NICOLE SALOW.</h2><h1>Let's work together!</h1>
     
     <body>
 
         <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
-
         <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
-
         <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-        <h2 id="test">Asset Compilation Test</h2>
-
+        
+        </br>
+        <h1 id="test">Asset Compilation Test</h1>
         <p>CSS test: <span class="test-css">text with blue border</span></p>
-
         <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
         
     </body>
