afbbe20efd93eb2eff72aa1914713b0a4210e9c4
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-04-19 07:59:43 -0500

    Update photography.blade.php

--- source/photography.blade.php
+++ source/photography.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1>Hello! My name is Nicole Salow. Let's work together!</h1>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
+    <p>These are some photos that I've taken...</p>
 
 @endsection
