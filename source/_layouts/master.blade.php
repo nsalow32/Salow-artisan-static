8550e386072968bfe67e2a2c158b9a1a2e4deca3
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-02 17:41:55 -0500

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -16,7 +16,7 @@
 
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}" type= "text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script type ="text/javascript" src="../source/_assets/js/nav.js"></script>
+    <script type ="text/javascript" src="http://www.nicolesalow.com/source/_assets/js/nav.js"></script>
 
 </head>
 
