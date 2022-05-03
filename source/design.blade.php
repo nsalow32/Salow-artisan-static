94e586406f4087546eb84ce8e9eab8a4a6f86044
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-03 11:11:52 -0500

    Update design.blade.php

--- source/design.blade.php
+++ source/design.blade.php
@@ -6,7 +6,7 @@
 
    <h1>DESIGN PORTFOLIO</h1>
    
-   <img src='https://github.com/nsalow32/Salow-artisan-static/blob/3edfd20c97d464fa5364752e92a33dde259e66a4/source/_assets/test.jpg' alt='test photo' />
+   <img src='source/_assets/test.jpg' alt='test photo' />
    
 
  
