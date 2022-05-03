0789afece4a554becb3e7d08ee0fd6b81f8900ec
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-03 11:02:33 -0500

    Update design.blade.php

--- source/design.blade.php
+++ source/design.blade.php
@@ -9,13 +9,13 @@
 
   <?php <h1>DESIGN PORTFOLIO</h1>
 
-  /**/  <ul>
- /**/      @forelse ($posts->sortBy('title') as $post)
-  /**/          <li>
-  /**/              <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
-   /**/             <small>{{ $post->prettyDate() }}</small>
-   /**/         </li>
-   /**/     @empty
-    /**/        <p>No posts to show.</p>
-   /**/     @endforelse
-  /**/  </ul>
+  /**/  <ul>//
+ /**/      @forelse ($posts->sortBy('title') as $post)//
+  /**/          <li>//
+  /**/              <a href="{{ $post->getPath() }}">{{ $post->title }}</a>//
+   /**/             <small>{{ $post->prettyDate() }}</small>//
+   /**/         </li>//
+   /**/     @empty//
+    /**/        <p>No posts to show.</p>//
+   /**/     @endforelse//
+  /**/  </ul>//
