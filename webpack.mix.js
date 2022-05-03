edc5aa4b810d8b27523c0c838bc50f2493cfc98b
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-03 11:32:32 -0500

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,8 +5,8 @@ mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
-    .js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css')
+    .js('source/assets/js/main.js', 'js')
+    .sass('source/assets/sass/main.scss', 'css')
     .options({
         processCssUrls: false,
     })
