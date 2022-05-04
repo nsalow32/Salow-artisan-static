bbf09b2132a3e621d93d78c49d9abf7a810f1403
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-04 15:40:44 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,11 +11,6 @@
         <p><b>ABOUT ME<b> </p>
         <p> I am forever optimistic, work hard, and love new adventures! Graphic design is my passion, and I also love photography, music and travel. I     strive to consistently learn new creative skills to achieve my personal and professional goals–I'm currently working towards a master's degree in computer science, and a certificate in UX Design. I have a wide array of professional and life experience that has shaped the person I am today: adaptable, patient, creative, energetic, and ready to jump in with both feet!
         </p>
-
-      
-       
-
-        
     </body>
     
 @endsection
