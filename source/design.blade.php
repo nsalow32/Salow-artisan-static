7084e06d440eb4b996e6e7b688d9eaee5f94fdbb
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-04 17:21:38 -0500

    Update design.blade.php

--- source/design.blade.php
+++ source/design.blade.php
@@ -7,6 +7,9 @@
    <h1>DESIGN PORTFOLIO</h1>
    
    <img src='source/_assets/images/testimage.png' alt='test photo' />
+
+   <link rel="testimage" href="{{ mix('image/testimage.jpg', 'assets/build') }}">
+
    
 
  
