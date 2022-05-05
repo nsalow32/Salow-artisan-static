ad7dfdaf7f5def9d2526b81282ade34340936f62
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-05 10:33:01 -0500

    testing images again

--- source/design.blade.php
+++ source/design.blade.php
@@ -6,9 +6,9 @@
 
    <h1>DESIGN PORTFOLIO</h1>
    
+   source/_assets/images/testimage.png
 
-
-   
+   <img src="testimage.png" alt="HTML tutorial" style="width:42px;height:42px;"></a>
 
  
 @endsection
