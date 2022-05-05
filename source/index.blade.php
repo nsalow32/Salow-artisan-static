1f8b4b4f6a894afb9df0bc4d9f4e8c2f5124393a
Nicole <90110876+nsalow32@users.noreply.github.com>
2022-05-04 19:46:06 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -6,14 +6,14 @@
         <p>Hello! My name is <b>NICOLE SALOW</b>. Let's work together!</p>
         </br>
         
-        <p> This site is currently under construction. Please visit my temporary portfolio site 
+        <h1> This site is currently under construction. Please visit my temporary portfolio site 
             
         
         <a href="https://nsalow.wixsite.com/nicolesalow">here</a>
-        </p>
+</h1>
         <!--<p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
 -->
-        <p><b>ABOUT ME<b> </p>
+        <p><b>ABOUT ME</b> </p>
         <p> I am forever optimistic, work hard, and love new adventures! Graphic design is my passion, and I also love photography, music and travel. I     strive to consistently learn new creative skills to achieve my personal and professional goals—I'm currently working towards a master's degree in computer science, and a certificate in UX Design. I have a wide array of professional and life experience that has shaped the person I am today: adaptable, patient, creative, energetic, and ready to jump in with both feet!
         </p>
     </body>
